8f440c1a56829bc27a9b1c7f0d3b333d85d4a51b
codewithsaidul <codewithsaidul@gmail.com>
2024-11-17 22:03:42 +0600

    Add New User Data API Completed

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 
@@ -10,6 +11,15 @@ const port = process.env.PORT || 4000;
 // ============ Middleware =============================
 app.use(cors());
 app.use(express.json());
+
+
+// ============ Authentication Middleware =============================
+//  Verify a User is authenticated ===========
+app.post('/authentication', async(req, res) =>{
+  const userEmail = req.body;
+  const token = jwt.sign(userEmail, process.env.ACCESS_TOKEN, { expiresIn: "30d"});
+  res.send( { token } );
+})
 
 // ==================== MongoDB =============================
 const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lggjuua.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -24,9 +34,33 @@ const client = new MongoClient(url, {
 });
 
 
+
+
+// Databes Connection
 const dbConnect = async () => {
-    try {
-        client.connect();
+  try {
+    client.connect();
+    
+    // ======================== Database Collection =============================
+    const userCollection = client.db('gadgetShop').collection('users');
+    const productCollection = client.db('gadgetShop').collection('products');
+
+
+    // ======================== User Related API =================
+    // Add New User
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email};
+      const existingUser = await userCollection.findOne(query);
+
+      if(existingUser) {
+        return res.status(400).send( { message: 'Email already exists' } );
+      }
+
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
+
         console.log("Connected to MongoDB!");
     } catch (error) {
         console.log(error); 
